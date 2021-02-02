79d03849f1c14490cded4b305cc2c79217d031f8
freust14 <freust14@gmail.com>
2021-02-01 18:58:40 -0600

    first commit

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        // TODO: Declare and initialize a CD and a DVD object.
+        // TODO: Declare and initialize a CD and a DVD object. test
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
